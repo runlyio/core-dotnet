79cbab3990d693c66d797af42c420fdfd7d8d35e
Chad Lee <git@chadly.net>
2020-02-05 09:24:18 -0600

    Allow multithreaded census importer

--- examples/Census/PlaceImporter.cs
+++ examples/Census/PlaceImporter.cs
@@ -2,34 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CsvHelper;
+using Microsoft.Extensions.Logging;
 
 namespace Runly.Examples.Census
 {
 	public class PlaceImporter : Process<CensusConfig, Place>, IDisposable
 	{
 		readonly IDownloader downloader;
+		readonly ILogger<PlaceImporter> logger;
 		CsvReader csv;
 
-		public PlaceImporter(CensusConfig config, IDownloader downloader)
+		public PlaceImporter(CensusConfig config, IDownloader downloader, ILogger<PlaceImporter> logger)
 			: base(config)
 		{
 			this.downloader = downloader;
-
-			// we want to stream the CSV file
-			Options.CanCountItems = false;
+			this.logger = logger;
 		}
 
 		public override async Task InitializeAsync()
 		{
 			var data = await downloader.Download("national_places.txt");
+
 			var reader = new StreamReader(data);
 			csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 			csv.Configuration.Delimiter = "|";
 		}
 
-		public override Task<IEnumerable<Place>> GetItemsAsync() => Task.FromResult(csv.GetRecords<Place>());
+		public override Task<IEnumerable<Place>> GetItemsAsync()
+		{
+			logger.LogInformation("Parsing CSV file...");
+
+			// use ToList to parse the entire file right here so we can process the items on multiple threads
+			IEnumerable<Place> items = csv.GetRecords<Place>().ToList();
+
+			return Task.FromResult(items);
+		}
 
 		public override Task<Result> ProcessAsync(Place place)
 		{
