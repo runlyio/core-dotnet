f98a8bdc86e17f147e50cef969b1f9ae5b45e7c2
William D. Sossamon <will@runly.io>
2020-07-14 17:35:55 -0500

    TestRun doc

--- src/Runly/Testing/TestRun.cs
+++ src/Runly/Testing/TestRun.cs
@@ -5,20 +5,45 @@ using System.Threading.Tasks;
 
 namespace Runly.Testing
 {
+	/// <summary>
+	/// A test run of a job, built by a <see cref="TestHost{TJob}"/>.
+	/// </summary>
+	/// <typeparam name="TJob">The type of <see cref="IJob"/> being tested.</typeparam>
 	public class TestRun<TJob> : IDisposable
 		where TJob : IJob
 	{
 		CancellationTokenSource cts;
 
+		/// <summary>
+		/// The <see cref="IServiceProvider"/> used to run the <see cref="Job"/>.
+		/// </summary>
 		public IServiceProvider Services { get; }
+
+		/// <summary>
+		/// The job being tested.
+		/// </summary>
 		public TJob Job { get; }
+
+		/// <summary>
+		/// The <see cref="Execution"/> implementation that ran the job.
+		/// </summary>
 		public Execution Execution { get; }
+
+		/// <summary>
+		/// A log of results emitted from the job during execution.
+		/// </summary>
 		public ResultLog Results { get; }
 
 		internal TestRun(TJob job, Execution execution, ResultLog results, ServiceProvider services) => (Job, Execution, Results, Services) = (job, execution, results, services);
 
+		/// <summary>
+		/// Performs clean up.
+		/// </summary>
 		public void Dispose() => (Services as IDisposable)?.Dispose();
 
+		/// <summary>
+		/// Triggers cancellation of a running job.
+		/// </summary>
 		public void Cancel()
 		{
 			if (cts == null)
@@ -28,9 +53,9 @@ namespace Runly.Testing
 		}
 
 		/// <summary>
-		/// Runs the job
+		/// Runs the job.
 		/// </summary>
-		/// <param name="cancellationToken">The <see cref="CancellationToken"/> to use</param>
+		/// <param name="cancellationToken">The <see cref="CancellationToken"/> to trigger cancellation.</param>
 		/// <param name="cancelAfter">When a positive value is given, cancels the job after that many items have been processed. This is useful to stop a service after an expected number of items have been processed in a unit test.</param>
 		public async Task<TestRun<TJob>> RunAsync(CancellationToken cancellationToken = default, int cancelAfter = 0)
 		{
