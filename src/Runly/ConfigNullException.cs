21ffbaed7e2bb407635ab09ec920b89e7dc25a53
William D. Sossamon <will@runly.io>
2020-07-14 21:36:36 -0500

    ConfigException doc

--- src/Runly/ConfigNullException.cs
+++ src/Runly/ConfigNullException.cs
@@ -1,7 +1,14 @@
 ﻿namespace Runly
 {
+	/// <summary>
+	/// Exception thrown when there is a null config parameter that cannot be null.
+	/// </summary>
 	public class ConfigNullException : ConfigException
 	{
+		/// <summary>
+		/// Initializes a new <see cref="ConfigNullException"/>.
+		/// </summary>
+		/// <param name="paramName">The config parameter that caused the exception.</param>
 		public ConfigNullException(string paramName)
 			: base($"Config parameter cannot be null: {paramName}", paramName) { }
 	}
