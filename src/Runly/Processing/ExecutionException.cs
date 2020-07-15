958aa75ef2e884f1a1dcd3840eb93afbd07ecbe9
William D. Sossamon <will@runly.io>
2020-07-15 08:45:39 -0500

    ExecutionException doc

--- src/Runly/Processing/ExecutionException.cs
+++ src/Runly/Processing/ExecutionException.cs
@@ -2,8 +2,16 @@
 
 namespace Runly.Processing
 {
+	/// <summary>
+	/// An exception used internally in <see cref="Execution"/>.
+	/// </summary>
 	public class ExecutionException : Exception
 	{
+		/// <summary>
+		/// Initializes a new <see cref="ExecutionException"/>..
+		/// </summary>
+		/// <param name="pointOfFailure">The location where a failure occured.</param>
+		/// <param name="innerException">The underlying cause of this exception.</param>
 		public ExecutionException(JobMethod pointOfFailure, Exception innerException)
 			: base($"Job failed at {pointOfFailure.ToString()}", innerException) { }
 	}
