0f89f162dd2759b0dfa586ae536ad2bc311efbd4
William D. Sossamon <will@runly.io>
2020-07-20 18:26:19 -0500

    Remove old command line parser stuff

--- src/Runly/ServiceExtensions.cs
+++ src/Runly/ServiceExtensions.cs
@@ -1,8 +1,5 @@
-using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Namotion.Reflection;
 using Runly.Internal;
 using System;
 using System.Collections.Generic;
@@ -10,7 +7,6 @@ using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
