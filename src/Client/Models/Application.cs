7a63acb01f2780172e903b01c735ea0b63cfe15e
Chad Lee <git@chadly.net>
2020-08-28 09:31:34 -0500

    Split Application ApiKey to SecretKey and PublishableKey

--- src/Client/Models/Application.cs
+++ src/Client/Models/Application.cs
@@ -6,9 +6,8 @@ namespace Runly.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string ApiKey { get; set; }
-
-		public bool IsPublic { get; set; }
+		public string SecretKey { get; set; }
+		public string PublishableKey { get; set; }
 
 		public DateTime CreatedAt { get; set; }
 		public DateTime? LastUsedAt { get; set; }
