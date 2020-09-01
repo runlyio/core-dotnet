57dd4f877071c9817a8f67d051a86ce88ab19d92
William Sossamon <will@willsossamon.com>
2020-08-31 20:23:26 -0500

    Move more stuff to API

--- src/Client/IAuthentication.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Runly
-{
-	public interface IAuthentication
-	{
-		Task<AccessToken> Authenticate(string clientId, string clientSecret, string audience, string authority = null);
-	}
-}
